34a045a9eaf841c5560d742ea99a5a8d45e57e15
João Victor Marques Andreotti <jvmandreotti@usp.br>
2023-01-21 18:59:07 +0100

    Fixes

--- src/labeling.cpp
+++ src/labeling.cpp
@@ -4,31 +4,27 @@
 
 Labeling::Labeling(){
     const char kNLetters = 26;
-    for (auto i = 0; i < kNLetters; i++){
-        std::unordered_map<Vertex, Label> map;
-        labels_[i] = map;
-    }
+    labels_.resize(kNLetters);
 }
 
 void Labeling::labelVertex(const Vertex& v, std::string label){
-    // const char kNLetters = 26;
-    // for(auto i = 0; i < kNLetters; i++)
-    //     labels_[i][v] = Label::kNoLetter;
-    // for(auto i = 0ul; i < label.length(); i++)
-    //     labels_[label[i] - 'A'][v] = Label::kHasLetter;
+    const char kNLetters = 26;
+    for(auto i = 0; i < kNLetters; i++)
+        labels_[i][v] = Label::kNoLetter;
+    for(auto i = 0ul; i < label.length(); i++)
+        labels_[label[i] - 'A'][v] = Label::kHasLetter;
 }
 
 void Labeling::labelVertex(const Vertex& v){
-    // const char kNLetters = 26;
-    // for(auto i = 0; i < kNLetters; i++)
-    //     labels_[i][v] = Label::kEmpty;
+    const char kNLetters = 26;
+    for(auto i = 0; i < kNLetters; i++)
+        labels_[i][v] = Label::kEmpty;
 }
 
 const Label& Labeling::get(const Vertex &v, char letter) const {
-    return Label::kEmpty;
-    // return labels_[letter - 'A'][v];
+    return labels_[letter - 'A'][v];
 }
-Label & Labeling::get(const Vertex &v, char letter){
-    Label l = Label::kEmpty;
-    return l;
+
+Label& Labeling::get(const Vertex &v, char letter){
+    return Labeling::get(v, letter);
 }
--- src/tree.cpp
+++ src/tree.cpp
@@ -2,10 +2,6 @@
 
 Tree::Tree(size_t n_vertexes){
     neighbours_.resize(n_vertexes);
-    for(auto i = 0ul; i < n_vertexes; i++){
-        std::list<Vertex> l;
-        neighbours_[i] = l;
-    }
 }
 
 std::list<Vertex> Tree::getNeighbours(Vertex v) const {
@@ -18,7 +14,7 @@ void Tree::addEdge(Vertex v1, Vertex v2){
 }
 
 Vertex Tree::findARoot(const Labeling& labeling) const{
-    for(auto i = 0ul; i < neighbours_.size(); i++)
-        if(labeling.get(i, 'a') == Label::kEmpty) return i;
+    for (auto i = 0ul; i < neighbours_.size(); i++)
+        if (labeling.get(i, 'a') == Label::kEmpty) return i;
     return 0ull;
 }
