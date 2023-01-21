0ff66980aa1baf6c717abb15b7b8cbb174c115c8
guilevieiram <guilhermevmanhaes@gmail.com>
2023-01-21 16:06:16 +0100

    makefile

---
+++ CMakeFiles/OptimalTreeLabeling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OptimalTreeLabeling.dir/src/label_optimally.cpp.o"
+  "libOptimalTreeLabeling.pdb"
+  "libOptimalTreeLabeling.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OptimalTreeLabeling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/OptimalTreeLabeling.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OptimalTreeLabeling.
+# This may be replaced when dependencies are built.
